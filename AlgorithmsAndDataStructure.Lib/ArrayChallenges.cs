641165b3692d5be68cdb0903cfa65c7b16e5f091
Gustavo Valente <guvalentepereira@gmail.com>
2024-11-19 19:59:25 -0300

    Feature: Adiciona ArrayChallenge no repositório e Projeto com XUnit

---
+++ AlgorithmsAndDataStructure.Lib/ArrayChallenges.cs
@@ -0,0 +1,51 @@
+﻿namespace AlgorithmsAndDataStructure.Lib
+{
+    public static class ArrayChallenges
+    {
+        public static int GreatestAverageReview(int[][] reviews)
+        {
+            reviews = reviews.OrderBy(x => x[0]).ToArray();
+
+            int[][] reviewCounter = new int[reviews.DistinctBy(x => x[0]).Count()][];
+            int reviewCounterIndex = -1;
+
+            int lastEvaluatedReviewsIndex = -1;
+
+            double greatestAverage = double.MinValue;
+            int indexOfGA = -1;
+
+            for (int i = 0; i < reviews.Length; i++)
+            {
+                if (reviews[i][0] != lastEvaluatedReviewsIndex)
+                {
+                    reviewCounterIndex++;
+                }
+
+                if (IsNewIndex(reviewCounter, reviews[i][0]))
+                {
+                    reviewCounter[reviewCounterIndex] = new int[] { reviews[i][0], 1, reviews[i][1]};
+                }
+                else
+                {
+
+                    reviewCounter[reviewCounterIndex] = new int[] { reviews[i][0], reviewCounter[reviewCounterIndex][1]+1, reviewCounter[reviewCounterIndex][2]+reviews[i][1]};
+                }
+
+                if ((double)reviewCounter[reviewCounterIndex][2]/reviewCounter[reviewCounterIndex][1] > greatestAverage)
+                {
+                    greatestAverage = (double)reviewCounter[reviewCounterIndex][2] / reviewCounter[reviewCounterIndex][1];
+                    indexOfGA = reviewCounterIndex;
+                }
+
+                lastEvaluatedReviewsIndex = reviews[i][0];
+            };
+
+            return reviewCounter[indexOfGA][0];
+        }
+
+        private static bool IsNewIndex(int[][] reviews, int index)
+        {
+            return reviews.FirstOrDefault(x => x!=null && x[0] == index) == default;
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructure.Test/ArrayChallengesTest.cs
@@ -0,0 +1,37 @@
+﻿using AlgorithmsAndDataStructure.Lib;
+
+namespace NumbersAPI.PrimeEngineLib.Test
+{
+    public class ArrayChallengesTest
+    {
+        private List<(int[][], int)> GreatestAverageReviewTestCases {  get; set; }
+
+        public ArrayChallengesTest()
+        {
+            GreatestAverageReviewTestCases = new List<(int[][], int)>() 
+            {
+                (new int[][]
+                {
+                    new int[]{9521024, 3},
+                    new int[]{9551223, 2},
+                    new int[]{9521024, 4},
+                    new int[]{5124521, 3},
+                    new int[]{8754615, 2},
+                    new int[]{5124521, 4},
+                }, 5124521)
+            };
+        }
+
+        [Fact]
+        public void GreatestAverageReview()
+        {
+            foreach(var testCase in GreatestAverageReviewTestCases)
+            {
+                //Função de média de avaliação
+                int result = ArrayChallenges.GreatestAverageReview(testCase.Item1);
+
+                Assert.Equal(testCase.Item2, result);
+            }
+        }
+    }
+}
